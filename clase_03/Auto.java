2dbb0c7e5e9ed3361c2d1a830e5c81e1157463da
nivergarah-collab <ni.vergarah@duocuc.cl>
2026-08-17 09:52:43 -0400

    Clase_03

---
+++ clase_03/Auto.java
@@ -0,0 +1,69 @@
+package clase_03;
+//Directo guia 1.2.4
+public class Auto {
+
+    private String modelo;
+    private String marca;
+    private String color;
+    private Integer ano;
+    private Integer km;
+    private String tipo;
+
+    public Auto(String modelo, String marca, String color, Integer ano, Integer km, String tipo) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.color = color;
+        this.ano = ano;
+        this.km = km;
+        this.tipo = tipo;
+    }
+
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Integer getKm() {
+        return km;
+    }
+
+    public void setKm(Integer km) {
+        this.km = km;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ clase_03/main.java
@@ -0,0 +1,4 @@
+package clase_03;
+
+public class main {
+}
