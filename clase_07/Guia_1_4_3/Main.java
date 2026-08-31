2aafdbf707c6e54098f48504d75eb63fef4ebd34
nivergarah-collab <ni.vergarah@duocuc.cl>
2026-08-31 09:57:04 -0400

    fina clase 07 correccion profe

--- clase_07/Guia_1_4_3/Main.java
+++ clase_07/Guia_1_4_3/Main.java
@@ -1,5 +1,8 @@
 package clase_07.Guia_1_4_3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Curso curso = new Curso("Programación");
@@ -8,6 +11,6 @@ public class Main {
         curso.mostrarEstudiantes();
         System.out.println(curso.obtenerEstudiante(5));}
         //aca hago lal ista
+        List<Prestamo> prestamo = new ArrayList<Prestamo>();
 
     }
-}
--- clase_07/Guia_1_4_3/Prestamo.java
+++ clase_07/Guia_1_4_3/Prestamo.java
@@ -4,12 +4,33 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Prestamo {
-    private HashMap<String, String> Estudiantes;
-    private HashSet<String> Libros;
+    private HashMap<String, String> estudiantes;
+    private HashSet<String> libros;
 
-    public Prestamo(HashMap<String, String> estudiantes, HashSet<String> libros) {
-        Estudiantes = estudiantes;
-        Libros = libros;
+    public Prestamo() {
+        this.estudiantes = new HashMap<>();
+        this.libros = new HashSet<>();
     }
+
+    public HashMap<String, String> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(HashMap<String, String> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public HashSet<String> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(HashSet<String> libros) {
+        this.libros = libros;
+    }
+
+    // crear metodo agregar estudiante
+    // crear metodo agregar libro
+
+
 
 }
